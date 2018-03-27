e03284f892aeaa47ff3ab48d721536206ad7e601
Valfredo <Valfredo@192.168.200.136>
2018-03-26 21:54:49 -0300

    Criação da persitencia viagemDao

---
+++ ProjetoMedianeira/src/persistencia/ViagemDao.java
@@ -0,0 +1,88 @@
+
+package persistencia;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Motorista;
+import model.Viagem;
+
+public class ViagemDao implements ICrudDao<Viagem>{
+
+    @Override
+    public void inserir(Viagem t) throws SQLException {
+        Connection con = util.Conexao.getConexao();
+        
+        
+        String sql = "INSERT INTO viagem (data_saida, turno, de, ate, id, onibus, motorista) VALUES (?,?,?,?,?,?,?)";
+        
+        PreparedStatement ps = con.prepareStatement(sql);
+        //ps.setString(1, t.getDataSaida());
+        ps.setBoolean(2, t.getTurno());
+        ps.setString(3, t.getDe());
+        ps.setString(4, t.getAte());
+        ps.setInt(5, t.getId());
+        ps.setString(6, t.getOnibus().getNumero());
+        ps.setInt(7, t.getMotorista().getId());
+        ps.execute();
+    }
+
+    @Override
+    public void deletar(Object... object) throws SQLException {
+              Connection con = util.Conexao.getConexao();
+        
+        String sql = "DELETE FROM viagem WHERE id=?;";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, (int) object[0]);
+        ps.execute();
+    }
+
+    @Override
+    public void alterar(Viagem t) throws SQLException {
+        Connection con = util.Conexao.getConexao();
+        String sql = "UPDATE viagem SET data_saida=?, turno=?, de=?, ate=?, id=?, onibus=?, motorista=?, WHERE id=?;";
+        
+        PreparedStatement ps = con.prepareStatement(sql);
+          //ps.setString(1, t.getDataSaida());
+        ps.setBoolean(2, t.getTurno());
+        ps.setString(3, t.getAte());
+      ps.setString(3, t.getDe());
+        ps.setString(4, t.getAte());
+        ps.setInt(5, t.getId());
+        ps.setString(6, t.getOnibus().getNumero());
+        ps.setInt(7, t.getMotorista().getId());
+        ps.execute();
+    }
+
+    @Override
+    public Viagem visualizarUm(Object... object) throws SQLException {
+             Connection con = util.Conexao.getConexao();
+        
+        String sql = "SELECT * FROM viagem WHERE id=?;";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, (int) object[0]);
+        ResultSet rs = ps.executeQuery();
+        
+        while (rs.next()) {
+            return new viagem(rs.getDate("data_saida"), rs.getBoolean("turno"), rs.getString("de"),rs.getString("ate"),rs.getInt("id"),rs.getString("onibus"), rs.getInt("motorista"));
+        }
+        return null;
+    }
+
+    @Override
+    public List<Viagem> visualizarAll() throws SQLException {
+        Connection con = util.Conexao.getConexao();
+        
+        String sql = "SELECT * FROM viagem ORDER BY id;";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+        
+        List<Viagem> lista = new ArrayList<>();
+        while (rs.next()) {
+            lista.add(new Viagem(rs.getDate("data_saida"), rs.getBoolean("turno"), rs.getString("de"),rs.getString("ate"),rs.getInt("id"),rs.getString("onibus"), rs.getInt("motorista"));
+        }
+        return lista;
+    }
+}
